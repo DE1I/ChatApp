9a5793f7dc653aef9bb327485d12d948f11e505c
Sean Andrei Benedicto <seanan256@gmail.com>
2026-08-03 13:59:56 +0800

    added the authenication models

---
+++ ChatApp/Models/RegisterViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatApp.Models
+{
+    public class RegisterViewModel
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? PasswordConfirmation { get; set; }
+    }
+}
